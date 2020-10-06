50c67af69259d58ac471af777efd553da7bc9623
Mehmet-Afacan <mehmetafacan42@hotmail.com>
2020-10-06 20:59:36 +0100

    added more awesome stuff

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,13 @@
+public class AwesomeTest {
+
+    //red means it is untracked
+    //green means it is under staging area
+    //grey in .idea folder means it is ignored
+    //when the color is normal, it means it is committed no changes
+
+    //I just pulled the new change from the github that I did not have locally
+    //and now my project is in sync with remote repository
+
+    //Now I made more awesome changes and I am about to PUSH this back to github
+
+}
